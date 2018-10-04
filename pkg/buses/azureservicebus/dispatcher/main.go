9ba473ab4d1df166170c77d7a0d4504214a93b5e
Andy Liu <andyliuliming@outlook.com>
2018-10-04 17:11:44 +0800

    fix

--- pkg/buses/azureservicebus/dispatcher/main.go
+++ pkg/buses/azureservicebus/dispatcher/main.go
@@ -1,4 +1,9 @@
 package main
 
+import "github.com/knative/pkg/signals"
+
 func main() {
+	// set up signals so we handle the first shutdown signal gracefully
+	stopCh := signals.SetupSignalHandler()
+	<-stopCh
 }
